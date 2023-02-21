298aa07ff092b2259f0f5e12ee941921a023a715
alepsdev <alessandropazdasilva@gmail.com>
2023-02-20 23:29:32 -0300

    adicionado busca

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,106 @@
-import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import SearchBar from "./components/searchBar";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import swapi from './axios/config';
 
 
-function App() {
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  const handleSearchChange = event => {
+    setSearch(event.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
+
+  const handleSearchSubmit = event => {
+    event.preventDefault();
+    setLoading(true);
+    swapi
+      .get(`people/?search=${search}`)
+      .then(response => {
+        setData(response.data.results);
+        setLoading(false);
+        setError(null);
+        setSearch('');
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+        setData([]);
+      });
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    swapi
+      .get('people/')
+      .then(response => {
+        setData(response.data.results);
+        setLoading(false);
+        setError(null);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+        setData([]);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <a href="/"><img src="./src/assets/png/star-wars-logo.png" height='160'/></a>
+        <p>Loading...</p>
+      </>
+    )
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
-    <div className="App">
-      <NavBar/>
-      <SearchBar/>
-      <div className="container">
-        <Outlet/>
-      </div>
+    <div>
+      <a href="/"><img src="./src/assets/png/star-wars-logo.png" height='160'/></a>
+      <form onSubmit={handleSearchSubmit}>
+        <input type="text" value={search} onChange={handleSearchChange} onKeyPress={handleKeyPress} placeholder="Nome do personagem..."/>
+      </form>
+      <ul>
+        {data.length == 1 ? (
+          data.map(item => (
+            <li key={item.url}>
+              <h1>{item.name}</h1>
+              <ul>
+                <li>Aniversário: {item.birth_year}</li>
+                <li>Gênero: {item.gender}</li>
+                <li>Cor dos olhos: {item.eye_color}</li>
+                <li>Filmes:</li>
+                {/* {item.films.map(film => (
+                  <ul>
+                    <li>{film}</li>
+                  </ul>
+                ))} */}
+              </ul>
+            </li>
+          ))
+        ): (
+          data.map(item => (
+            <li key={item.url}>
+              <h3>{item.name}</h3>
+            </li>            
+          ))
+        )}
+        
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import {createBrowserRouter, RouterProvider, Route} from 'react-router-dom';
-import Home from "./routes/Home"
 
 const router = createBrowserRouter([
   {
     element: <App/>,
-    children: [
-      {
-        path: "/",
-        element: <Home/>
-      }
-    ]
+    path: "/"
   }
 ])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
